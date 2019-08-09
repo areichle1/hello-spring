86ca601e21d4eabdc89db8784e7597360dca1b43
Andrea Reichle <andrea_reichle@yahoo.com>
2019-08-09 12:48:25 -0500

    completed for initial prep work

---
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -0,0 +1,15 @@
+package org.launchcode.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
