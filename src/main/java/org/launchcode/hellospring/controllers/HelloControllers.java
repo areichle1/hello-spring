26cb3b516bcf8dedfdd80466fbf0d9288ec4f0d8
Andrea Reichle <andrea_reichle@yahoo.com>
2019-08-19 09:36:44 -0500

    complete studio 3, Bonjour spring!, so the user can now be greeted in 5 different languages

--- src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControllers.java
@@ -1,12 +1,14 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 public class HelloControllers {
@@ -25,26 +27,52 @@ public class HelloControllers {
         return "Hello " + name;
     }
 
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloForm() {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloForm(Model model) {
+        ArrayList<String> languages = new ArrayList<>();
+        languages.add("English");
+        languages.add("Spanish");
+        languages.add("French");
+        languages.add("Italian");
+        languages.add("German");
 
-        String html = "<form method= 'post'>" +
-                "<input type= 'text' name='name' />" +
-                "<input type='submit' value='Greet Me!' />" +
-                "</form>";
+        model.addAttribute("languages", languages);
 
-        return html;
+        return "hello";
     }
 
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @RequestMapping(value = "/hello", method = RequestMethod.POST)
     @ResponseBody
     public String helloPost(HttpServletRequest request) {
 
         String name = request.getParameter("name");
+        String language = request.getParameter("language");
 
-        return "Hello " + name;
+        // init a temp variable to store the final language value
+        String greeting;
+        // set the language value based off of what was passed on the request
+        switch (language) {
+            case "English":
+                greeting = "Hello";
+                break;
+            case "Spanish":
+                greeting = "Hola";
+                break;
+            case "French":
+                greeting = "Bonjour";
+                break;
+            case "Italian":
+                greeting = "Ciao";
+                break;
+            case "German":
+                greeting = "Hallo";
+                break;
+            default:
+                greeting = "Hello, Hola, Bonjour";
 
+        }
+        // return correct response
+        return greeting + " " + name;
     }
 
     @RequestMapping(value = "hello/{name}")
